b89f7d77ecca0d886469b6a2152b78735ab105f1
thetien <tien.nguyenthe@amela.vn>
2024-08-01 15:40:49 +0700

    setup initial project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    devSourcemap: true,
+},
+server: {
+    port: 8080,
+},
+resolve: {
+    alias: {
+        src: path.resolve(__dirname, './src'),
+    },
+},
 })
