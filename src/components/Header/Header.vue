cf54315749ec83c3fd9cbd408af2d8c0654cbfe8
thetien <tien.nguyenthe@amela.vn>
2024-08-16 15:05:45 +0700

    task/home_page

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="maxContent bg-red-500">
+        header
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/layouts/UserLayout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Header from 'src/components/Header/Header.vue';
+
+
+</script>
+
+<template>
+    <div>
+        <Header />
+        <main>
+            <RouterView />
+        </main>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
