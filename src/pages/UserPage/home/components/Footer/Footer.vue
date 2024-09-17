6c7fd6d436d0b58e782abe10261e75da1b22e3f8
the tien <tien.nguyenthe@amela.vn>
2024-09-17 23:32:57 +0700

    30b6801

---
+++ src/pages/UserPage/home/components/Footer/Footer.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import FacebookIcon from "src/assets/icons/FacebookIcon.vue";
+</script>
+
+<template>
+  <section class="py-[120px] bg-[#171717]">
+    <div class="maxContent flex items-center flex-col justify-center">
+      <div class="flex gap-4">
+        <div
+          class="w-[45px] h-[45px] flex items-center justify-center transition-all duration-500 bg-transparent rounded-[50%] hover:bg-bgPrimaryLinear"
+        >
+          <FacebookIcon class="text-white" />
+        </div>
+        <div
+          class="w-[45px] h-[45px] flex items-center justify-center transition-all duration-500 bg-transparent rounded-[50%] hover:bg-bgPrimaryLinear"
+        >
+          <FacebookIcon class="text-white" />
+        </div>
+        <div
+          class="w-[45px] h-[45px] flex items-center justify-center transition-all duration-500 bg-transparent rounded-[50%] hover:bg-bgPrimaryLinear"
+        >
+          <FacebookIcon class="text-white" />
+        </div>
+        <div
+          class="w-[45px] h-[45px] flex items-center justify-center transition-all duration-500 bg-transparent rounded-[50%] hover:bg-bgPrimaryLinear"
+        >
+          <FacebookIcon class="text-white" />
+        </div>
+        <div
+          class="w-[45px] h-[45px] flex items-center justify-center transition-all duration-500 bg-transparent rounded-[50%] hover:bg-bgPrimaryLinear"
+        >
+          <FacebookIcon class="text-white" />
+        </div>
+      </div>
+      <div class="mt-2 text-center">
+        © 2024 MegaOne. Made With Love By Themesindustry
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
